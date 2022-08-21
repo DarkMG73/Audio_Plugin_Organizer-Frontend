5054ede3e3a09ff97aee2524f4b25f5eed7adb5d
DarkMG73 <levelthreeemail@gmail.com>
2022-08-21 14:24:30 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -9,47 +9,109 @@ import { useSelector, useDispatch } from "react-redux";
 import BarLoader from "./UI/Loaders/BarLoader/BarLoader";
 import { getUserCookie, getUserUserByToken } from "./storage/userDB";
 import { authActions } from "./store/authSlice";
-import { ErrorBoundary } from "./Components/ErrorBoundary/ErrorBoundary";
+import { ErrorBoundary } from "./Components/ErrorHandling/ErrorBoundary/ErrorBoundary";
+import CardPrimary from "./UI/Cards/CardPrimary/CardPrimary";
+
 const App = () => {
   const [user, setUser] = useState(false);
   const toolsData = useSelector((state) => state.toolsData);
 
   const dispatch = useDispatch();
-
   useEffect(() => {
-    getUserCookie().then((res) => {
-      if (res.status >= 400) {
-        GatherToolData().then((data) => {
-          // console.log("🟣 | getData | questionsFromDB", data);
-          dispatch(audioToolDataActions.initState(data));
-        });
-      } else {
-        getUserUserByToken(res.data.cookie).then((userProfile) => {
-          setUser({ ...userProfile, token: res.data.cookie });
-        });
-      }
-    });
+    getUserCookie()
+      .then((res) => {
+        if (res.status >= 400) {
+          GatherToolData()
+            .then((data) => {
+              console.log("🟣 | getData | questionsFromDB", data);
+              dispatch(audioToolDataActions.initState(data));
+            })
+            .catch((err) => {
+              console.log(
+                "%c --> %cline:34%cerr----->",
+                "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+                "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+                "color:#fff;background:rgb(254, 67, 101);padding:3px;border-radius:2px",
+                err
+              );
+              if (err.status >= 500) {
+
+                const message = 
+                  " *** " +
+                    err.statusText +
+                    " *** It looks like we can not make a connection. Here is the error we received: Please make sure there is an internet connection and refresh the browser. if the problem continues, please send a quick email so this can be looked into. Email Address: general@glassinteractive.com.com"
+                    error     ErrorElement(message)
+                
+              } else {
+                alert(
+                  "Oh no! Something went wrong. Please try again or contact general@glassinteractive.com with the following information if the problem continues -->  " +
+                    err.status +
+                    " |" +
+                    err.statusText +
+                    " | " +
+                    err.request.responseURL
+                );
+              }
+            });
+        } else {
+          getUserUserByToken(res.data.cookie).then((userProfile) => {
+            setUser({ ...userProfile, token: res.data.cookie });
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(
+          "%c --> %cline:34%cerr----->",
+          "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+          "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+          "color:#fff;background:rgb(254, 67, 101);padding:3px;border-radius:2px",
+          err
+        );
+        alert(" An error: ", err.toString());
+      });
   }, []);
 
   useEffect(() => {
     if (user) {
       dispatch(authActions.logIn(user));
-      GatherToolData(user).then((data) => {
-        // console.log("🟣 | getData | questionsFromDB", data);
-        dispatch(audioToolDataActions.initState(data));
-      });
+      GatherToolData(user)
+        .then((data) => {
+          console.log("🟣 | getData | questionsFromDB", data);
+          dispatch(audioToolDataActions.initState(data));
+        })
+        .catch((err) => {
+          console.log(
+            "%c --> %cline:34%cerr----->",
+            "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+            "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+            "color:#fff;background:rgb(254, 67, 101);padding:3px;border-radius:2px",
+            err
+          );
+          if (err.status >= 500) {
+            alert(
+              " *** " +
+                err.statusText +
+                " *** It looks like we can not make a connection. Here is the error we received: Please make sure there is an internet connection and refresh the browser. if the problem continues, please send a quick email so this can be looked into. Email Address: general@glassinteractive.com.com"
+            );
+          } else {
+            alert(
+              "Oh no! Something went wrong. Please try again or contact general@glassinteractive.com if the problem continues. Here is the data: " +
+                err.status +
+                " |" +
+                err.statusText +
+                " | " +
+                err.request.responseURL
+            );
+          }
+        });
     }
   }, [user]);
 
   return (
     <div className={styles["app-container"]}>
       <ErrorBoundary>
-        <h1>
-          This is a test
-          <input type="checkbox" value="the value" checked />
-        </h1>
+        <Header />
       </ErrorBoundary>
-      <Header />
       {user.isAdmin && (
         <Fragment>
           <div className={styles["admin-notice"]}>
@@ -59,10 +121,8 @@ const App = () => {
         </Fragment>
       )}
       <div className={styles["content-container"]}>
-        {!toolsData.allTools && <BarLoader />}
-        <ErrorBoundary specialProps={"Testing Props"}>
-          {toolsData.allTools && <Home />}
-        </ErrorBoundary>
+        {/* !toolsData.allTools && <BarLoader />*/}
+        <ErrorBoundary>{/*toolsData.allTools &&*/ <Home />}</ErrorBoundary>
       </div>
     </div>
   );
