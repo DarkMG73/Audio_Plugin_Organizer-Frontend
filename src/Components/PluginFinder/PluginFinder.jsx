e9575a781d0f629fe12b1dcdd1caff20690771e3
DarkMG73 <levelthreeemail@gmail.com>
2024-11-05 23:10:39 -0500

    2024-11-06T04:10:39.154Z:

--- src/Components/PluginFinder/PluginFinder.jsx
+++ src/Components/PluginFinder/PluginFinder.jsx
@@ -1,221 +1,259 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import {useSelector} from 'react-redux'
-import Styles from "./PluginFinder.module.css"
-import {getLocalPluginData} from '../../storage/audioToolsDB'
+import React, { useState, useEffect, Fragment } from "react";
+import { useSelector } from "react-redux";
+import Styles from "./PluginFinder.module.css";
+import { getLocalPluginData } from "../../storage/audioToolsDB";
 import BarLoader from "../../UI/Loaders/BarLoader/BarLoader";
 import AddAToolForm from "../AddATool/AddAToolForm";
 import useGroomDataForToolForm from "../../Hooks/useGroomDataForToolForm";
 
-
-
 const PluginFinder = () => {
-  const {allTools} = useSelector((state) => state.toolsData);
-  const acceptedPluginWrappers = ['vst','vst3','component']
-  const [fileNames, setFileNames] = useState([]);
-  const [addToLibrary, setAddToLibrary] = useState([])
-  const [userFilesToGroomArray, setUserFilesToGroomArray] = useState(false)
-  const [sendToLibrary, setSendToLibrary] = useState(false)
-  const [findNewPlugins, setFindNewPlugins] = useState(false);
-  const groomDataForToolForm = useGroomDataForToolForm()
-  const toolsSchema = useSelector((state) => state.toolsData.toolsSchema);
-  const [activateLoader, setActivateLoader] = useState(false)
+   const { allTools } = useSelector((state) => state.toolsData);
+   const acceptedPluginWrappers = ["vst", "vst3", "component"];
+   const [fileNames, setFileNames] = useState([]);
+   const [addToLibrary, setAddToLibrary] = useState([]);
+   const [userFilesToGroomArray, setUserFilesToGroomArray] = useState(false);
+   const [sendToLibrary, setSendToLibrary] = useState(false);
+   const [findNewPlugins, setFindNewPlugins] = useState(false);
+   const groomDataForToolForm = useGroomDataForToolForm();
+   const toolsSchema = useSelector((state) => state.toolsData.toolsSchema);
+   const [activateLoader, setActivateLoader] = useState(false);
 
+   useEffect(() => {
+      if (findNewPlugins) {
+         setActivateLoader(true);
+         getLocalPluginData()
+            .then((data) => {
+               console.log("plugin names---->", data);
 
-    
-  useEffect(()=>{
-    if(findNewPlugins) {
-      setActivateLoader(true)
-      getLocalPluginData().then(
-          data=>{
-              console.log('plugin names---->',data)
+               const acceptedPluginNames = new Set();
 
-              const acceptedPluginNames = new Set()
-              
-              data.forEach(name=>{
-                const nameArray =  name.split('.') 
+               data.forEach((name) => {
+                  const nameArray = name.split(".");
 
-                const isValid =  acceptedPluginWrappers.includes(nameArray[name.split('.').length-1] )
+                  const isValid = acceptedPluginWrappers.includes(
+                     nameArray[name.split(".").length - 1]
+                  );
 
-                if(isValid) acceptedPluginNames.add(nameArray[0])
-                }
-              ) 
-              
-              const matchedNames = []
-              const groomedList = []
-              // Remove existing plugin names
-              for ( const name of  acceptedPluginNames ) {
-              
-                  if(!matchedNames.includes(name)){
-                    for (const value of  Object.values(allTools)) {
-                      const referenceID = value.masterLibraryID || value.name
-                      
-                      if(name.replaceAll(' ','').includes(referenceID.replaceAll(' ',''))) {
-                        matchedNames.push(name)
-                        break
-                      }               
+                  if (isValid) acceptedPluginNames.add(nameArray[0]);
+               });
+
+               const matchedNames = [];
+               const groomedList = [];
+               // Remove existing plugin names
+               for (const name of acceptedPluginNames) {
+                  if (!matchedNames.includes(name)) {
+                     for (const value of Object.values(allTools)) {
+                        const referenceID = value.masterLibraryID || value.name;
+
+                        if (
+                           name
+                              .replaceAll(" ", "")
+                              .includes(referenceID.replaceAll(" ", ""))
+                        ) {
+                           matchedNames.push(name);
+                           break;
+                        }
+                     }
                   }
-                }
-                
-                if(!matchedNames.includes(name)) { 
-                  groomedList.push(name) 
-                }
-              }
 
-            setFileNames(groomedList)
+                  if (!matchedNames.includes(name)) {
+                     groomedList.push(name);
+                  }
+               }
 
-            setActivateLoader(false)
-          }
-      ).catch(err=>{
+               setFileNames(groomedList);
 
-            setActivateLoader(false)
-          console.log('err->', err)
-        })} else{
-          setActivateLoader(false)
-        }
-  }, [findNewPlugins])
-
-
-  useEffect(()=>{
-    console.log('%c⚪️►►►► %cline:77%caddToLibrary', 'color:#fff;background:#ee6f57;padding:3px;border-radius:2px', 'color:#fff;background:#1f3c88;padding:3px;border-radius:2px', 'color:#fff;background:rgb(38, 157, 128);padding:3px;border-radius:2px', addToLibrary)
-  },[addToLibrary])
-
-  useEffect(()=>{
-    if(sendToLibrary) {
-        const categoryTitles = Object.keys(toolsSchema)
-        console.log('%c⚪️►►►► %cline:84%ccategoryTitles', 'color:#fff;background:#ee6f57;padding:3px;border-radius:2px', 'color:#fff;background:#1f3c88;padding:3px;border-radius:2px', 'color:#fff;background:rgb(178, 190, 126);padding:3px;border-radius:2px', categoryTitles)
-
-        const toAddArrays = addToLibrary.map(name=>{
-          const outputArray = []
-        
-          outputArray[categoryTitles.indexOf('name')] =  name
-          outputArray[categoryTitles.indexOf('status')] =  'active'
-          outputArray[categoryTitles.indexOf('rating')] =  '3'
-          outputArray[categoryTitles.indexOf('masterLibraryID')] =  name
-                    console.log('%c⚪️►►►► %cline:91%coutputArray', 'color:#fff;background:#ee6f57;padding:3px;border-radius:2px', 'color:#fff;background:#1f3c88;padding:3px;border-radius:2px', 'color:#fff;background:rgb(222, 125, 44);padding:3px;border-radius:2px', outputArray)
-          return outputArray
-        })
-
-        const groomedData = groomDataForToolForm([categoryTitles, ...toAddArrays])
-        
-        setUserFilesToGroomArray(groomedData)
-        setSendToLibrary(false)
-  }
-  setActivateLoader(false)
-  },[sendToLibrary])
-
-  ////////////////////////////////////////
-  /// HANDLERS
-  ////////////////////////////////////////
-  const handleCheckBox = (e) => {
-    e.preventDefault();
-    const name = e.target.dataset.fileName
-    if(addToLibrary.includes(name) ) {
-      setAddToLibrary(prevState=>{
-        const oldState = [...prevState]
-        const newState = oldState.filter(item=>item!==name)
-        return newState
-      })
-    }
-      else {
-        setAddToLibrary(prevState=>[...prevState, name])
+               setActivateLoader(false);
+            })
+            .catch((err) => {
+               setActivateLoader(false);
+               console.log("err->", err);
+            });
+      } else {
+         setActivateLoader(false);
       }
-  }
+   }, [findNewPlugins]);
 
-  const handleToggleAllCheckBox = (e) => {
-    e.preventDefault();
-    if(addToLibrary.length>0) {
-      setAddToLibrary([])
-    }
-      else {
-        setAddToLibrary([...fileNames])
+   useEffect(() => {
+      console.log(
+         "%c⚪️►►►► %cline:77%caddToLibrary",
+         "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+         "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+         "color:#fff;background:rgb(38, 157, 128);padding:3px;border-radius:2px",
+         addToLibrary
+      );
+   }, [addToLibrary]);
+
+   useEffect(() => {
+      if (sendToLibrary) {
+         const categoryTitles = Object.keys(toolsSchema);
+         console.log(
+            "%c⚪️►►►► %cline:84%ccategoryTitles",
+            "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+            "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+            "color:#fff;background:rgb(178, 190, 126);padding:3px;border-radius:2px",
+            categoryTitles
+         );
+
+         const toAddArrays = addToLibrary.map((name) => {
+            const outputArray = [];
+
+            outputArray[categoryTitles.indexOf("name")] = name;
+            outputArray[categoryTitles.indexOf("status")] = "active";
+            outputArray[categoryTitles.indexOf("rating")] = "3";
+            outputArray[categoryTitles.indexOf("masterLibraryID")] = name;
+            console.log(
+               "%c⚪️►►►► %cline:91%coutputArray",
+               "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+               "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+               "color:#fff;background:rgb(222, 125, 44);padding:3px;border-radius:2px",
+               outputArray
+            );
+            return outputArray;
+         });
+
+         const groomedData = groomDataForToolForm([
+            categoryTitles,
+            ...toAddArrays
+         ]);
+
+         setUserFilesToGroomArray(groomedData);
+         setSendToLibrary(false);
       }
-  }
+      setActivateLoader(false);
+   }, [sendToLibrary]);
 
-  const handleFindNewPluginsButton = (e)=>{
-    setActivateLoader(true)
-    setFindNewPlugins(!findNewPlugins)
-  }
-  const handleAddToLibraryButton =()=>{
-    setActivateLoader(true)
-    setSendToLibrary(true)
+   ////////////////////////////////////////
+   /// HANDLERS
+   ////////////////////////////////////////
+   const handleCheckBox = (e) => {
+      e.preventDefault();
+      const name = e.target.dataset.fileName;
+      if (addToLibrary.includes(name)) {
+         setAddToLibrary((prevState) => {
+            const oldState = [...prevState];
+            const newState = oldState.filter((item) => item !== name);
+            return newState;
+         });
+      } else {
+         setAddToLibrary((prevState) => [...prevState, name]);
+      }
+   };
 
-  }
+   const handleToggleAllCheckBox = (e) => {
+      e.preventDefault();
+      if (addToLibrary.length > 0) {
+         setAddToLibrary([]);
+      } else {
+         setAddToLibrary([...fileNames]);
+      }
+   };
 
+   const handleFindNewPluginsButton = (e) => {
+      setActivateLoader(true);
+      setFindNewPlugins(!findNewPlugins);
+   };
+   const handleAddToLibraryButton = () => {
+      setActivateLoader(true);
+      setSendToLibrary(true);
+   };
 
-  ////////////////////////////////////////
-  /// Styles
-  ////////////////////////////////////////
-  const submitButtonStyles = {
-    position: "relative",
-    top: "-0",
-    left: "40%",
-    width: "80%",
-    transform: " translateX(-50%)",
-    background: "var(--iq-color-accent-gradient)",
-    borderRadius: "50px",
-    height: "3em",
-    font: "var(--iq-font-heading-2)",
-  };
+   ////////////////////////////////////////
+   /// Styles
+   ////////////////////////////////////////
+   const submitButtonStyles = {
+      position: "relative",
+      top: "-0",
+      left: "40%",
+      width: "80%",
+      transform: " translateX(-50%)",
+      background: "var(--iq-color-accent-gradient)",
+      borderRadius: "50px",
+      height: "3em",
+      font: "var(--iq-font-heading-2)"
+   };
 
-  const buttonStyles = {
-    width: "80%",
-    borderRadius: "50px",
-    height: "3em",
-    font: "var(--iq-font-heading-2)",
-    fontSize: "1.5em",
-    padding: "0",
-    textTransform: "uppercase",
-    fontWeight: "900",
-    letterSpacing: "0.25em",
-    textShadow:
-      "rgb(0 0 0 / 50%) -1px -1px 1px, rgb(255 255 255 / 50%) 1px 1px 1px, 0 0 22px wheat",
-  };
+   const buttonStyles = {
+      width: "80%",
+      borderRadius: "50px",
+      height: "3em",
+      font: "var(--iq-font-heading-2)",
+      fontSize: "1.5em",
+      padding: "0",
+      textTransform: "uppercase",
+      fontWeight: "900",
+      letterSpacing: "0.25em",
+      textShadow:
+         "rgb(0 0 0 / 50%) -1px -1px 1px, rgb(255 255 255 / 50%) 1px 1px 1px, 0 0 22px wheat"
+   };
 
+   ////////////////////////////////////////
+   /// Output
+   ////////////////////////////////////////
+   return (
+      <div className={Styles["plugin-finder-container"]}>
+         <button onClick={handleFindNewPluginsButton}>Find New Plugins</button>
+         <button onClick={handleAddToLibraryButton}>
+            Add to Library &rarr;
+         </button>
 
-  ////////////////////////////////////////
-  /// Output
-  ////////////////////////////////////////
-  return (
-    <div className={Styles['plugin-finder-container']}>
-   <button onClick={handleFindNewPluginsButton}>Find New Plugins</button>
-   <button onClick={handleAddToLibraryButton}>Add to Library &rarr;</button>
-      <ul>
-         {activateLoader > 0 &&
-            <div key="loader" className={Styles["loader-wrap"]}>
-              <BarLoader />
+         {userFilesToGroomArray && (
+            <div
+               key="add-a-tool-inputs-container 3"
+               className={Styles["inputs-container"]}
+            >
+               <AddAToolForm
+                  saveOrUpdateData="save"
+                  formData={userFilesToGroomArray}
+                  buttonStyles={buttonStyles}
+                  submitButtonStyles={submitButtonStyles}
+               />
             </div>
-          }
-         
-            <label htmlFor="select-all" onClick={handleToggleAllCheckBox}>   <input type="checkbox" name="select-all" />Select All</label>
-        {findNewPlugins &&
-        <Fragment>
-          {fileNames.length > 0 && <h3>{fileNames.length}</h3>}
-          {userFilesToGroomArray && (
-                <div
-                  key="add-a-tool-inputs-container 3"
-                  className={Styles["inputs-container"]}
-                >
-                  <AddAToolForm
-                    saveOrUpdateData="save"
-                    formData={userFilesToGroomArray}
-                    buttonStyles={buttonStyles}
-                    submitButtonStyles={submitButtonStyles}
-                  />
-                </div>
-          )}
-          {fileNames.map((fileName, index) => (
-              <li key={index}>
+         )}
+         <div className={Styles["plugin-finder-selector-wrap"]}>
+            <ul>
+               {activateLoader > 0 && (
+                  <div key="loader" className={Styles["loader-wrap"]}>
+                     <BarLoader />
+                  </div>
+               )}
 
-                <label htmlFor={fileName} onClick={handleCheckBox} data-file-name={fileName} >                <input type="checkbox" name={fileName}  data-file-name={fileName} defaultChecked={addToLibrary.includes(fileName)} checked={addToLibrary.includes(fileName)}/>
-                {fileName}</label>
-              </li>
-            ))
-          }
-    </Fragment>
-        }
-      </ul>
-    </div>
-  );
-}
+               <label htmlFor="select-all" onClick={handleToggleAllCheckBox}>
+                  {" "}
+                  <input type="checkbox" name="select-all" />
+                  Select All
+               </label>
+               {findNewPlugins && (
+                  <Fragment>
+                     {fileNames.length > 0 && <h3>{fileNames.length}</h3>}
 
-export default PluginFinder
+                     {fileNames.map((fileName, index) => (
+                        <li key={index}>
+                           <label
+                              htmlFor={fileName}
+                              onClick={handleCheckBox}
+                              data-file-name={fileName}
+                           >
+                              {" "}
+                              <input
+                                 type="checkbox"
+                                 name={fileName}
+                                 data-file-name={fileName}
+                                 defaultChecked={addToLibrary.includes(
+                                    fileName
+                                 )}
+                                 checked={addToLibrary.includes(fileName)}
+                              />
+                              {fileName}
+                           </label>
+                        </li>
+                     ))}
+                  </Fragment>
+               )}
+            </ul>
+         </div>
+      </div>
+   );
+};
+
+export default PluginFinder;
