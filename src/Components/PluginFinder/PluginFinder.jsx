ddd8285ff3b7f969ac457a80c5801940c54825b2
DarkMG73 <levelthreeemail@gmail.com>
2024-11-12 11:29:25 -0500

    Added plugin paths to Plugin Finder.

--- src/Components/PluginFinder/PluginFinder.jsx
+++ src/Components/PluginFinder/PluginFinder.jsx
@@ -5,8 +5,17 @@ import { getLocalPluginData } from "../../storage/audioToolsDB";
 import BarLoader from "../../UI/Loaders/BarLoader/BarLoader";
 import AddAToolForm from "../AddATool/AddAToolForm";
 import useGroomDataForToolForm from "../../Hooks/useGroomDataForToolForm";
+import { updateUserPluginPaths } from "../../storage/userDB";
 
 const PluginFinder = () => {
+   const { user } = useSelector((state) => state.auth);
+   console.log(
+      "%c⚪️►►►► %cline:11%cuser",
+      "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+      "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+      "color:#fff;background:rgb(39, 72, 98);padding:3px;border-radius:2px",
+      user
+   );
    const { allTools } = useSelector((state) => state.toolsData);
    const acceptedPluginWrappers = ["vst", "vst3", "component"];
    const [fileNames, setFileNames] = useState([]);
@@ -14,14 +23,46 @@ const PluginFinder = () => {
    const [userFilesToGroomArray, setUserFilesToGroomArray] = useState(false);
    const [sendToLibrary, setSendToLibrary] = useState(false);
    const [findNewPlugins, setFindNewPlugins] = useState(false);
+   const [pluginPathsObj, setPluginPathsObj] = useState({});
+   const [showPluginPathSaveButton, setShowPluginPathSaveButton] =
+      useState(true);
+   const [showHidePluginPathsButton, setShowHidePluginPathsButton] =
+      useState(false);
+   const [showPluginPaths, setShowPluginPaths] = useState(false);
+   const [noPluginPathsExist, setNoPluginPathsExist] = useState(true);
    const groomDataForToolForm = useGroomDataForToolForm();
    const toolsSchema = useSelector((state) => state.toolsData.toolsSchema);
    const [activateLoader, setActivateLoader] = useState(false);
 
    useEffect(() => {
+      let pluginPathsExist = false;
+
+      if (pluginPathsObj) {
+         Object.values(pluginPathsObj).forEach((path) => {
+            if (!pluginPathsExist && path) pluginPathsExist = true;
+         });
+      }
+
+      if (pluginPathsExist) setNoPluginPathsExist(false);
+   }, [pluginPathsObj]);
+
+   useEffect(() => {
+      const userPluginPaths =
+         user &&
+         Object.hasOwn(user, "pluginPaths") &&
+         user.pluginPaths.constructor === Object
+            ? user.pluginPaths
+            : {};
+
+      setShowHidePluginPathsButton(true);
+      setPluginPathsObj(userPluginPaths);
+   }, [user]);
+
+   useEffect(() => {
       if (findNewPlugins) {
          setActivateLoader(true);
-         getLocalPluginData()
+
+         getLocalPluginData(user, pluginPathsObj)
             .then((data) => {
                console.log("plugin names---->", data);
 
@@ -62,15 +103,20 @@ const PluginFinder = () => {
                }
 
                setFileNames(groomedList);
-
-               setActivateLoader(false);
+               setTimeout(() => {
+                  setActivateLoader(false);
+               }, 3000);
             })
             .catch((err) => {
-               setActivateLoader(false);
+               setTimeout(() => {
+                  setActivateLoader(false);
+               }, 3000);
                console.log("err->", err);
             });
       } else {
-         setActivateLoader(false);
+         setTimeout(() => {
+            setActivateLoader(false);
+         }, 3000);
       }
    }, [findNewPlugins]);
 
@@ -99,12 +145,19 @@ const PluginFinder = () => {
          setUserFilesToGroomArray(groomedData);
          setSendToLibrary(false);
       }
-      setActivateLoader(false);
+      setTimeout(() => {
+         setActivateLoader(false);
+      }, 3000);
    }, [sendToLibrary, addToLibrary, groomDataForToolForm, toolsSchema]);
 
    ////////////////////////////////////////
    /// HANDLERS
    ////////////////////////////////////////
+   const handleShowPluginPaths = (e) => {
+      e.preventDefault();
+      setShowPluginPaths(!showPluginPaths);
+   };
+
    const handleCheckBox = (e) => {
       e.preventDefault();
       const name = e.target.dataset.fileName;
@@ -117,6 +170,28 @@ const PluginFinder = () => {
       } else {
          setAddToLibrary((prevState) => [...prevState, name]);
       }
+   };
+
+   const handleAddPluginLocationInput = (e) => {
+      e.preventDefault();
+      setShowPluginPaths(true);
+      setShowPluginPathSaveButton(true);
+      setPluginPathsObj((prevState) => {
+         const newState = { ...prevState };
+         newState[e.target.name] = e.target.value;
+         return newState;
+      });
+   };
+
+   const handleSaveLocationsButton = (e) => {
+      console.log(
+         "%c⚪️►►►► %cline:143%chandleSaveLocationsButton",
+         "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+         "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+         "color:#fff;background:rgb(95, 92, 51);padding:3px;border-radius:2px",
+         pluginPathsObj
+      );
+      updateUserPluginPaths(user, pluginPathsObj);
    };
 
    const handleCheckAllCheckBox = (e) => {
@@ -201,6 +276,93 @@ const PluginFinder = () => {
                            computer.
                         </h3>
                      )}
+
+                     {showHidePluginPathsButton && (
+                        <button
+                           className={Styles["url-show-paths-button"]}
+                           onClick={handleShowPluginPaths}
+                        >
+                           Show Plugin Paths
+                        </button>
+                     )}
+
+                     {!showHidePluginPathsButton && (
+                        <p>
+                           * Please paste in the paths to each plugin location
+                           on your system *
+                        </p>
+                     )}
+                     {(showPluginPaths || noPluginPathsExist) && (
+                        <div className={Styles["url-input-container"]}>
+                           <div className={Styles["location-input-container"]}>
+                              <label htmlFor="location-1">
+                                 Plugin Location 1
+                              </label>
+                              <input
+                                 name="location-1"
+                                 type="text"
+                                 defaultValue={
+                                    Object.hasOwn(pluginPathsObj, "location-1")
+                                       ? pluginPathsObj["location-1"]
+                                       : ""
+                                 }
+                                 onChange={handleAddPluginLocationInput}
+                              />
+                           </div>{" "}
+                           <div className={Styles["location-input-container"]}>
+                              <label htmlFor="location-2">
+                                 Plugin Location 2
+                              </label>
+                              <input
+                                 name="location-2"
+                                 type="text"
+                                 defaultValue={
+                                    Object.hasOwn(pluginPathsObj, "location-2")
+                                       ? pluginPathsObj["location-2"]
+                                       : ""
+                                 }
+                                 onChange={handleAddPluginLocationInput}
+                              />{" "}
+                           </div>{" "}
+                           <div className={Styles["location-input-container"]}>
+                              <label htmlFor="location-3">
+                                 Plugin Location 3
+                              </label>
+                              <input
+                                 name="location-3"
+                                 type="text"
+                                 defaultValue={
+                                    Object.hasOwn(pluginPathsObj, "location-3")
+                                       ? pluginPathsObj["location-3"]
+                                       : ""
+                                 }
+                                 onChange={handleAddPluginLocationInput}
+                              />{" "}
+                           </div>{" "}
+                           <div className={Styles["location-input-container"]}>
+                              <label htmlFor="location-4">
+                                 Plugin Location 4
+                              </label>
+                              <input
+                                 name="location-4"
+                                 type="text"
+                                 defaultValue={
+                                    Object.hasOwn(pluginPathsObj, "location-4")
+                                       ? pluginPathsObj["location-4"]
+                                       : ""
+                                 }
+                                 onChange={handleAddPluginLocationInput}
+                              />
+                           </div>
+                           {showPluginPathSaveButton &&
+                              Object.keys(pluginPathsObj).length > 0 && (
+                                 <button onClick={handleSaveLocationsButton}>
+                                    Save Plugin Location Changes
+                                 </button>
+                              )}
+                        </div>
+                     )}
+
                      <div className={Styles["button-container"]}>
                         <label
                            htmlFor="select-none"
@@ -209,6 +371,7 @@ const PluginFinder = () => {
                            <input type="checkbox" name="select-none" />
                            Clear All
                         </label>
+
                         {fileNames.length > 20 && (
                            <label
                               htmlFor="select-twenty"
@@ -230,13 +393,15 @@ const PluginFinder = () => {
                            </label>
                         )}
 
-                        <label
-                           htmlFor="select-all"
-                           onClick={handleCheckAllCheckBox}
-                        >
-                           <input type="checkbox" name="select-all" />
-                           Select All
-                        </label>
+                        {fileNames.length > 0 && (
+                           <label
+                              htmlFor="select-all"
+                              onClick={handleCheckAllCheckBox}
+                           >
+                              <input type="checkbox" name="select-all" />
+                              Select All
+                           </label>
+                        )}
                      </div>
                      <button
                         className={Styles["add-to-library-button"]}
--- src/storage/audioToolsDB.js
+++ src/storage/audioToolsDB.js
@@ -2,204 +2,215 @@ import axios from "axios";
 
 /// GET THE PLUGINS/////////////////////////////
 export const getData = async (user) => {
-  let axiosConfig = null;
+   let axiosConfig = null;
 
-  if (user) {
-    axiosConfig = {
-      headers: {
-        "Content-Type": "text/plain",
-        Authorization: "JWT " + user.token,
-      },
-      timeout: 60000,
-    };
-  }
+   if (user) {
+      axiosConfig = {
+         headers: {
+            "Content-Type": "text/plain",
+            Authorization: "JWT " + user.token
+         },
+         timeout: 60000
+      };
+   }
 
-  const res = await axios.post("/api/all-plugins/", user, axiosConfig);
+   const res = await axios.post("/api/all-plugins/", user, axiosConfig);
 
-  return res.data;
+   return res.data;
 };
 
 /// GET ONE PLUGIN /////////////////////////////
 export const getPluginBy_Id = async (user, tool_Id) => {
-  let axiosConfig = null;
+   let axiosConfig = null;
 
-  if (user) {
-    axiosConfig = {
-      headers: {
-        "Content-Type": "text/plain",
-        Authorization: "JWT " + user.token,
-      },
-      timeout: 60000,
-    };
-  }
+   if (user) {
+      axiosConfig = {
+         headers: {
+            "Content-Type": "text/plain",
+            Authorization: "JWT " + user.token
+         },
+         timeout: 60000
+      };
+   }
 
-  const res = await axios.post(
-    "/api/all-plugins/" + tool_Id,
-    user,
-    axiosConfig
-  );
-  return res.data;
+   const res = await axios.post(
+      "/api/all-plugins/" + tool_Id,
+      user,
+      axiosConfig
+   );
+   return res.data;
 };
 
 /// GET LOCAL PLUGINS/////////////////////////////
-export const getLocalPluginData = async (user) => {
-  let axiosConfig = null;
+export const getLocalPluginData = async (user, pluginPathsObj) => {
+   console.log(
+      "%c⚪️►►►► %cline:45%cpluginPathsObj",
+      "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+      "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+      "color:#fff;background:rgb(3, 38, 58);padding:3px;border-radius:2px",
+      pluginPathsObj
+   );
+   let axiosConfig = null;
 
-  if (user) {
-    axiosConfig = {
-      headers: {
-        "Content-Type": "text/plain",
-        Authorization: "JWT " + user.token,
-      },
-      timeout: 60000,
-    };
-  }
+   if (user) {
+      axiosConfig = {
+         headers: {
+            "Content-Type": "text/plain",
+            Authorization: "JWT " + user.token
+         },
+         timeout: 60000
+      };
+   }
 
-  const res = await axios.get("/api/all-plugins/get-local-plugins/", user, axiosConfig);
+   const res = await axios.get(
+      "/api/all-plugins/get-local-plugins/",
+      { params: { ...pluginPathsObj } },
+      axiosConfig
+   );
 
-  return res.data;
+   return res.data;
 };
 
 /// SAVE ONE /////////////////////////////////////
 export async function savePlugin(userAndDataObject) {
-  const axiosConfig = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "JWT " + userAndDataObject.user.token,
-    },
-  };
+   const axiosConfig = {
+      headers: {
+         "Content-Type": "application/json",
+         Authorization: "JWT " + userAndDataObject.user.token
+      }
+   };
 
-  const response = await axios
-    .post(`/api/all-plugins/add/`, userAndDataObject, axiosConfig)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log("err", err);
-      // console.log("errors", err.response.data.err.message);
-      return err;
-    });
-  return response;
+   const response = await axios
+      .post(`/api/all-plugins/add/`, userAndDataObject, axiosConfig)
+      .then((res) => {
+         return res;
+      })
+      .catch((err) => {
+         console.log("err", err);
+         // console.log("errors", err.response.data.err.message);
+         return err;
+      });
+   return response;
 }
 
 /// SAVE MANY /////////////////////////////////
 export async function saveManyPlugins(userAndDataObject) {
-  const axiosConfig = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "JWT " + userAndDataObject.user.token,
-    },
-  };
+   const axiosConfig = {
+      headers: {
+         "Content-Type": "application/json",
+         Authorization: "JWT " + userAndDataObject.user.token
+      }
+   };
 
-  const response = await axios
-    .post(`/api/all-plugins/add-many/`, userAndDataObject, axiosConfig)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log("err", err);
-      // console.log("errors", err.response.data.err.message);
-      return err;
-    });
-  return response;
+   const response = await axios
+      .post(`/api/all-plugins/add-many/`, userAndDataObject, axiosConfig)
+      .then((res) => {
+         return res;
+      })
+      .catch((err) => {
+         console.log("err", err);
+         // console.log("errors", err.response.data.err.message);
+         return err;
+      });
+   return response;
 }
 
 /// UPDATE //////////////////////////////////
 export async function updateAPlugin(id, dataObj, user) {
-  const axiosConfig = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "JWT " + user.token,
-    },
-  };
+   const axiosConfig = {
+      headers: {
+         "Content-Type": "application/json",
+         Authorization: "JWT " + user.token
+      }
+   };
 
-  const response = await axios
-    .post(`/api/all-plugins/update/`, { dataObj }, axiosConfig)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log("err", err);
-      // console.log("errors", err.response.data.err.message);
-      return err;
-    });
-  return response;
+   const response = await axios
+      .post(`/api/all-plugins/update/`, { dataObj }, axiosConfig)
+      .then((res) => {
+         return res;
+      })
+      .catch((err) => {
+         console.log("err", err);
+         // console.log("errors", err.response.data.err.message);
+         return err;
+      });
+   return response;
 }
 
 /// DELETE ////////////////////////////////
 export async function deleteAPlugin(id, user) {
-  const axiosConfig = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "JWT " + user.token,
-    },
-  };
-  const response = await axios
-    .get(`/api/all-plugins/${id}/delete/`, axiosConfig)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log("err", err);
-      console.log("errors", err.response.data.err.message);
-      return err;
-    });
-  return response;
+   const axiosConfig = {
+      headers: {
+         "Content-Type": "application/json",
+         Authorization: "JWT " + user.token
+      }
+   };
+   const response = await axios
+      .get(`/api/all-plugins/${id}/delete/`, axiosConfig)
+      .then((res) => {
+         return res;
+      })
+      .catch((err) => {
+         console.log("err", err);
+         console.log("errors", err.response.data.err.message);
+         return err;
+      });
+   return response;
 }
 
 /// DELETE ALL ///////////////////////////
 export async function deleteAllPlugins(user) {
-  const axiosConfig = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "JWT " + user.token,
-    },
-  };
-  const response = await axios
-    .get(`/api/all-plugins/deleteAll/`, axiosConfig)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log(
-        "%c --> %cline:102%cerr",
-        "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-        "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-        "color:#fff;background:rgb(3, 38, 58);padding:3px;border-radius:2px",
-        err
-      );
-      console.log("err", err);
-      console.log("errors", err.response.data.err.message);
-      return err;
-    });
-  return response;
+   const axiosConfig = {
+      headers: {
+         "Content-Type": "application/json",
+         Authorization: "JWT " + user.token
+      }
+   };
+   const response = await axios
+      .get(`/api/all-plugins/deleteAll/`, axiosConfig)
+      .then((res) => {
+         return res;
+      })
+      .catch((err) => {
+         console.log(
+            "%c --> %cline:102%cerr",
+            "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+            "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+            "color:#fff;background:rgb(3, 38, 58);padding:3px;border-radius:2px",
+            err
+         );
+         console.log("err", err);
+         console.log("errors", err.response.data.err.message);
+         return err;
+      });
+   return response;
 }
 
 /// GET AUDIO PLUGIN SCHEMA //////////////
 export async function getSchemaForAudioPlugin() {
-  const axiosConfig = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  const output = await axios
-    .get(`/api/all-plugins/model/`, axiosConfig)
-    .then((res) => {
-      return res.data.model;
-    })
-    .catch((err) => {
-      const output = { response: { request: {} } };
-      console.log("err", err);
-      console.log("output", output);
-
-      if (err.code && err.code === "ERR_NETWORK") {
-        output.response.status = 500;
-        output.response.statusText = err.message;
-        output.response.request.responseURL = err.config.baseURL;
+   const axiosConfig = {
+      headers: {
+         "Content-Type": "application/json"
       }
-      return output.response;
-    });
+   };
 
-  return output;
+   const output = await axios
+      .get(`/api/all-plugins/model/`, axiosConfig)
+      .then((res) => {
+         return res.data.model;
+      })
+      .catch((err) => {
+         const output = { response: { request: {} } };
+         console.log("err", err);
+         console.log("output", output);
+
+         if (err.code && err.code === "ERR_NETWORK") {
+            output.response.status = 500;
+            output.response.statusText = err.message;
+            output.response.request.responseURL = err.config.baseURL;
+         }
+         return output.response;
+      });
+
+   return output;
 }
