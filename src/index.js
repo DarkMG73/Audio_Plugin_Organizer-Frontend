ab2804dee07e5e70bc70ab33dc3d0264709a20fa
DarkMG73 <levelthreeemail@gmail.com>
2024-12-11 09:11:27 -0500

    2024-12-11T14:11:27.959Z:

--- src/index.js
+++ src/index.js
@@ -14,69 +14,69 @@ import axios from "axios";
 ///////////////////////
 const inDevelopment = process.env.NODE_ENV === "development";
 console.log(
-  "%c --> %cline:15%cprocess.env.NODE_ENV",
-  "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-  "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-  "color:#fff;background:rgb(118, 77, 57);padding:3px;border-radius:2px",
-  process.env.NODE_ENV
+   "%c --> %cline:15%cprocess.env.NODE_ENV",
+   "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+   "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+   "color:#fff;background:rgb(118, 77, 57);padding:3px;border-radius:2px",
+   process.env.NODE_ENV
 );
 console.log(
-  "%c --> %cline:15%cinDevelopment",
-  "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-  "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-  "color:#fff;background:rgb(96, 143, 159);padding:3px;border-radius:2px",
-  inDevelopment
+   "%c --> %cline:15%cinDevelopment",
+   "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+   "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+   "color:#fff;background:rgb(96, 143, 159);padding:3px;border-radius:2px",
+   inDevelopment
 );
 // axios.defaults.baseURL = "https://api-audio-plugin-organizer.herokuapp.com/";
 axios.defaults.baseURL = "https://api-organizer.glassinteractive.com/";
 
 console.log(
-  "%c --> %cline:16%caxios.defaults.baseURL",
-  "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-  "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-  "color:#fff;background:rgb(229, 187, 129);padding:3px;border-radius:2px",
-  axios.defaults.baseURL
+   "%c --> %cline:16%caxios.defaults.baseURL",
+   "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+   "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+   "color:#fff;background:rgb(229, 187, 129);padding:3px;border-radius:2px",
+   axios.defaults.baseURL
 );
 
 if (inDevelopment) {
-  // axios.defaults.baseURL = "https://api-organizer.glassinteractive.com/";
-  axios.defaults.baseURL = "http://localhost:8000";
-  console.log(
-    "%cRunning in DEV MODE with the base URL:",
-    "color:#fff;background:#027482;padding:14px;border-radius:0 25px 25px 0",
-    axios.defaults.baseURL
-  );
+   axios.defaults.baseURL = "https://api-organizer.glassinteractive.com/";
+   // axios.defaults.baseURL = "http://localhost:8000";
+   console.log(
+      "%cRunning in DEV MODE with the base URL:",
+      "color:#fff;background:#027482;padding:14px;border-radius:0 25px 25px 0",
+      axios.defaults.baseURL
+   );
 }
 
 axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(
-  (request) => {
-    // alert("In REQ");
-    if (inDevelopment) console.log("request", request);
-    // Edit request config
-    return request;
-  },
-  (error) => {
-    // alert("In REQ Error");
-    if (inDevelopment) console.log(error);
-    return Promise.reject(error);
-  }
+   (request) => {
+      // alert("In REQ");
+      if (inDevelopment) console.log("request", request);
+      // Edit request config
+      return request;
+   },
+   (error) => {
+      // alert("In REQ Error");
+      if (inDevelopment) console.log(error);
+      return Promise.reject(error);
+   }
 );
 
 axios.interceptors.response.use(
-  (response) => {
-    // alert("In Res");
-    if (inDevelopment) console.log("RESPONSE---> ", response);
+   (response) => {
+      // alert("In Res");
+      if (inDevelopment) console.log("RESPONSE---> ", response);
 
-    return response;
-  },
+      return response;
+   },
 
-  (error) => {
-    if (inDevelopment) console.log(error);
-    return Promise.reject(error);
-  }
+   (error) => {
+      if (inDevelopment) console.log(error);
+      return Promise.reject(error);
+   }
 );
 
 ////////////////////////
@@ -84,14 +84,14 @@ axios.interceptors.response.use(
 ///////////////////////
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </Provider>
-    ,
-  </React.StrictMode>
+   <React.StrictMode>
+      <Provider store={store}>
+         <ErrorBoundary>
+            <App />
+         </ErrorBoundary>
+      </Provider>
+      ,
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
