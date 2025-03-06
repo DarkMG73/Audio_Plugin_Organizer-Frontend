4e899142e3cd9eecc5d35bc8d032e8e2a6570f14
DarkMG73 <levelthreeemail@gmail.com>
2025-03-06 14:21:12 -0500

    Fix mobile warning and Filters button. Improved loader.

--- src/UI/Loaders/BarLoader/BarLoader.js
+++ src/UI/Loaders/BarLoader/BarLoader.js
@@ -1,20 +1,24 @@
 import styles from "./BarLoader.module.css";
 
 function BarLoader() {
-  return (
-    <div className={styles["loader-wrap"]}>
-      <div className={styles["loader"] + " " + styles["loader2"]}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+   return (
+      <div className={styles["loader-wrap"] + " loader-container"}>
+         <div
+            className={
+               styles["loader"] + " " + styles["loader2"] + " loader loader2"
+            }
+         >
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default BarLoader;
