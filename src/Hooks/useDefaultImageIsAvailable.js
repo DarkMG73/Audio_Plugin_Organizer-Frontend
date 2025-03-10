8db350138e2c62d43218bc556322fc34bcbad606
DarkMG73 <levelthreeemail@gmail.com>
2025-03-10 17:36:45 -0400

    Fix bad functions name input error.

--- src/Hooks/useDefaultImageIsAvailable.js
+++ src/Hooks/useDefaultImageIsAvailable.js
@@ -1,44 +1,52 @@
 const useDefaultImageIsAvailable = () => {
-  const defaultImageIsAvailable = (functionsArray, defaultImgObj) => {
-    const cleanFunctionsArray = functionsArray.filter((item) => {
-      // Make str or arr
-      const eligibleTypes = ['String', 'Array'];
-      if (eligibleTypes.includes(item.constructor.name)) return item.length > 0;
-      return false;
-      //make sure str or arr is not empty
-    });
+   const defaultImageIsAvailable = (functionsArray, defaultImgObj) => {
+      const cleanFunctionsArray = functionsArray.filter((item) => {
+         // Make str or arr
+         const eligibleTypes = ["String", "Array"];
+         if (eligibleTypes.includes(item.constructor.name))
+            return item.length > 0;
+         return false;
+         //make sure str or arr is not empty
+      });
 
-    const groomName = (name) => name.replaceAll(' ', '-').toLowerCase();
-    const imageGetter = (indexOfcleanFuncArr) => {
-      return defaultImgObj[
-        groomName(cleanFunctionsArray[indexOfcleanFuncArr])
-      ][0].src;
-    };
+      const groomName = (name) =>
+         name && typeof name === "string"
+            ? name.replace(/ /g, "-").toLowerCase()
+            : "";
 
-    // See if image exists: "audio effects" is skipped to use the actual effect image.
-    if (cleanFunctionsArray.length > 0) {
-      if (
-        cleanFunctionsArray.length === 1 ||
-        groomName(cleanFunctionsArray[0]) !== 'audio-effects'
-      ) {
-        try {
-          return imageGetter(0);
-        } catch {
-          return false;
-        }
-      } else {
-        try {
-          return imageGetter(1);
-        } catch {
-          return false;
-        }
+      const imageGetter = (indexOfcleanFuncArr) => {
+         return defaultImgObj[
+            groomName(
+               cleanFunctionsArray[indexOfcleanFuncArr],
+               cleanFunctionsArray
+            )
+         ][0].src;
+      };
+
+      // See if image exists: "audio effects" is skipped to use the actual effect image.
+      if (cleanFunctionsArray.length > 0) {
+         if (
+            cleanFunctionsArray.length === 1 ||
+            groomName(cleanFunctionsArray[0]) !== "audio-effects"
+         ) {
+            try {
+               return imageGetter(0);
+            } catch {
+               return false;
+            }
+         } else {
+            try {
+               return imageGetter(1);
+            } catch {
+               return false;
+            }
+         }
       }
-    }
 
-    return false;
-  };
+      return false;
+   };
 
-  return defaultImageIsAvailable;
+   return defaultImageIsAvailable;
 };
 
 export default useDefaultImageIsAvailable;
